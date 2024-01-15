ecae50f0accb02a5dc16db040c3201d4cee6c6e4
Pedro Henrique Abreu <pedroabreu.dev@gmail.com>
2024-01-15 19:20:43 -0400

    Pokémon Detail Screen (1)

--- app/src/main/java/com/pedroabreudev/pokecompose/MainActivity.kt
+++ app/src/main/java/com/pedroabreudev/pokecompose/MainActivity.kt
@@ -5,14 +5,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.toLowerCase
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.pedroabreudev.pokecompose.pokemondetail.PokemonDetailScreen
 import com.pedroabreudev.pokecompose.pokemonlist.PokemonListScreen
 import com.pedroabreudev.pokecompose.ui.theme.PokeComposeTheme
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Locale
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -41,6 +44,11 @@ class MainActivity : ComponentActivity() {
                         val pokemonName = remember {
                             it.arguments?.getString("pokemonName")
                         }
+                        PokemonDetailScreen(
+                            dominantColor = dominantColor,
+                            pokemonName = pokemonName?.lowercase(Locale.ROOT) ?: "",
+                            navController = navController
+                        )
                     }
                 }
             }
